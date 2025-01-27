f257ca5a024060edf8957a12661839519f3d4e58
Cade Butler <131890765+c4de475@users.noreply.github.com>
2025-01-27 13:56:03 -0500

    Delete exercise4.c

--- exercise4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    float temp;
-    printf("Please enter a temperature: \n");
-    scanf("%f", &temp);
-
-    printf("The temperature entered is greater than 70 degrees or less than 85 fahrenheit (1 being true, 0 being false): %d\n", temp > 70 && temp < 85);
-}
