1b96093392fa1b3411cc091c84f5e386f455d3a1
Cade Butler <131890765+c4de475@users.noreply.github.com>
2025-01-27 14:27:02 -0500

    Print even numbers between 0 and 10 using while loop

---
+++ 2.2.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+void main() {
+    // Print even numbers between 0 and 10 using while loop
+
+    int i = 0;
+    while (i <= 10){
+        printf("%d \n", i);
+        i += 2;
+    }
+}
