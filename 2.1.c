055720124e7f722b150ec497a06f7daa383816d2
Cade Butler <131890765+c4de475@users.noreply.github.com>
2025-01-27 14:24:50 -0500

    Countdown from 10 to 1 and output “Happy New Year!” using while loop

---
+++ 2.1.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+void main(){
+    // Countdown from 10 to 1 and output “Happy New Year!” using while loop
+
+    int i = 10;
+    while (i > 0){
+        printf("%d \n", i);
+        i--;
+    }
+    printf("Happy New Year! \n");
+}
