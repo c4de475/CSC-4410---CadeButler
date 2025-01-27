0c9fac78273fd36c35221125c77df58bd6e6f869
Cade Butler <131890765+c4de475@users.noreply.github.com>
2025-01-27 14:28:37 -0500

    Write a program that counts to 100 by tens using for loop

---
+++ 2.3.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+void main () {
+    // Write a program that counts to 100 by tens using for loop
+
+    for (int i = 0; i <= 100; i += 10) {
+        printf("%d \n", i);
+    }
+}
